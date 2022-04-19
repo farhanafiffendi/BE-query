4cafdf00c09ab64c719171ed317a5181dc58dce2
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-04-19 21:22:54 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express()
+const router = require("./src/routes")
 
 const port = 5000
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+app.use(express.json())
+
+app.use("/api/v1/", router)
 
 
 app.listen(port, () => console.log(`server running on port: ${port}`))
