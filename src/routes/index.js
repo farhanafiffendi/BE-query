c20b46d7e3242a0c01a4c57564e3dc4aeb44c17e
farhanafiffendi <farhanafiffendi19@gmail.com>
2022-04-19 21:03:16 +0700

    update

---
+++ src/routes/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+//instantiate express module here
+const router = express.Router()
+
+// Controller
+const {
+    getTodos,
+    getTodo,
+    addTodo,
+    updateTodo,
+    deleteTodo,
+} = require("../controllers/todo");
+
+
+// Route
+router.get("/todos", getTodos);
+router.get("/todo/:id", getTodo);
+router.post("/todo", addTodo);
+router.patch("/todo/:id", updateTodo);
+router.delete("/todo/:id", deleteTodo);
+
+module.exports = router
